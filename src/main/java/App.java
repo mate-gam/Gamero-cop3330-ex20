261503630c5c784946167f0cd218e23ff416a6e4
mategam <gameromateo@knights.ucf.edu>
2021-09-11 16:52:06 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 20 Solution
+ *  Copyright 2021 Mateo Gamero
+ */
+import java.util.Scanner;
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What is the order amount? ");
+        double order = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("What state do you live in? ");
+        String state = sc.nextLine();
+        double tax = 0, total = 0, realTax = 0;
+        if (state.equals("Wisconsin"))
+        {
+            tax = 0.05;
+            System.out.print("What country do you live in? ");
+            String county = sc.nextLine();
+            if (county.equals("Eau Claire"))
+            {
+                tax += .004;
+            }
+            if (county.equals(("Dane")))
+            {
+                tax += .005;
+            }
+            realTax = order * tax;
+            total = order * (1 + tax);
+        }
+        if (state.equals("Illinois"))
+        {
+            tax = .008;
+            realTax = order * tax;
+            total = order * (1 + tax);
+        }
+        float realTotal = (float)Math.round(total * 100) / 100;
+        String realTotal1 = String.format("The subtotal is %.2f \nThe tax is %.2f \nThe total is %.2f", order, realTax, realTotal);
+        System.out.println(realTotal1);
+    }
+}
